240389eb22c443aff5e8323a56d2399b6ac0e844
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 01:55:04 -0300

    Create FreeShipping component

---
+++ src/components/FreeShipping.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const ShippingRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 8px;
+  width: 75%;
+  background-color: #c0ff9c;
+  color: #1e6f00;
+  border-radius: 16px;
+  font-weight: 600;
+`;
+
+const FreeShipping = () => (
+  <ShippingRow>
+    <span>Parabéns, sua compra tem frete grátis!</span>
+    {/* <div>
+      <span>Parabéns, sua compra</span>
+    </div>
+    <div>
+      <span>tem frete grátis!</span>
+    </div> */}
+  </ShippingRow>
+);
+
+export default FreeShipping;
--- src/pages/CartWithShipping.js
+++ src/pages/CartWithShipping.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import Button from '../components/Button';
+import FreeShipping from '../components/FreeShipping';
 import Header from '../components/Header';
 import theme from '../style/theme';
 
 const CartContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -22,9 +23,11 @@ const CartBox = styled.div`
   box-shadow: 0 0 32px 8px #dadce0;
   display: flex;
   flex-direction: column;
+  margin: 48px 24px;
 
   main {
     display: flex;
+    flex-direction: column;
     flex-grow: 1;
   }
 `;
@@ -35,15 +38,38 @@ const FooterRow = styled.div`
   align-items: center;
   border-top: 1px solid ${theme.borderColor};
   padding: 24px 0;
-`; 
+`;
+
+const SubTotalRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid ${theme.borderColor};
+  padding: 24px 0;
+`;
 
 const LoginPage = () => (
   <CartContainer>
     <CartBox>
       <Header />
-      <main></main>
+      <main>
+        <br/>
+        <p>oi</p>
+        <br/>
+        <p>oi</p>
+        <br/>
+        <p>oi</p>
+        <br/>
+        <p>oi</p>
+      </main>
+      <SubTotalRow>
+        <FreeShipping />
+      </SubTotalRow>
       <FooterRow>
-        <Button><span>Finalizar compra</span></Button>
+        <Button>
+          <span>Finalizar compra</span>
+        </Button>
       </FooterRow>
     </CartBox>
   </CartContainer>
