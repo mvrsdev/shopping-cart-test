74300e79793fe5dba733e37c834b632b991c070c
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 18:58:49 -0300

    refactor free shipping component display

--- src/components/FreeShipping.js
+++ src/components/FreeShipping.js
@@ -2,27 +2,19 @@ import styled from 'styled-components';
 import theme from '../style/theme';
 
 const ShippingRow = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  padding: 8px;
+  text-align: center;
+  padding: 12px 16px;
   margin-top: 24px;
   width: 75%;
   background-color: ${theme.freeShippingBackground};
   color: ${theme.freeShippingText};
-  border-radius: 16px;
+  border-radius: 2rem;
   font-weight: 600;
 `;
 
 const FreeShipping = () => (
   <ShippingRow>
-    <div>
-      <span style={{ paddingRight:4 }} >Parabéns, sua compra</span>
-    </div>
-    <div>
-      <span>tem frete grátis!</span>
-    </div>
+    Parabéns, sua compra tem frete grátis!
   </ShippingRow>
 );
 
