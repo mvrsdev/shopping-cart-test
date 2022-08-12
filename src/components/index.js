999c36e9ee0639bb4183a1339ac188fc3b7ab973
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 19:00:35 -0300

    add products list to index

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as Button } from './Button';
 export { default as FreeShipping } from './FreeShipping';
 export { default as Header } from './Header';
 export { default as Product } from './Product';
+export { default as ProductsList } from './ProductsList';
 export { default as SubTotal } from './SubTotal';
 
