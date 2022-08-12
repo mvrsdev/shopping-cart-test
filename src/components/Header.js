74102f4834637ab76e967a8c03916f5fa1a489ee
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 19:02:33 -0300

    refactor Header border

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import theme from '../style/theme';
 
 const HeaderRow = styled.div`
   display: flex;
@@ -9,7 +8,7 @@ const HeaderRow = styled.div`
   font-size: 24px;
   font-weight: 900;
   width: 100%;
-  border-bottom: 1px solid ${theme.borderColor};
+  border-bottom: 1px solid ${(props)=>props.theme.borderColor};
   padding: 24px 0 ;
 `;
 
