ea394c71ab294e990067892457dacfe4bbb47584
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 19:01:55 -0300

    Refactor App router logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { CartWithShipping, CartWithoutShipping } from './pages';
+import { ShoppingCart } from './pages';
 import theme from './style/theme';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CartWithShipping />} />
-        <Route path="without-shipping" element={<CartWithoutShipping />} />
+        <Route path="/cart" element={<ShoppingCart />} />
 
-        <Route path="*" element={<p>Page not Found</p>} />
+        <Route path="*" element={<Navigate to="/cart?type=standard" />} />
       </Routes>
     </BrowserRouter>
   </ThemeProvider>
