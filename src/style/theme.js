07083608c2fea219e1f3136dcc97e00b84360ef4
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 03:05:07 -0300

    refactor FreeShipping Component

--- src/style/theme.js
+++ src/style/theme.js
@@ -6,6 +6,8 @@ const theme = {
   buttonColor: '#1a73e8',
   backgroundBox: 'white',
   background: '#cbd2e1',
+  freeShippingBackground:'#c0ff9c',
+  freeShippingText:'#1e6f00',
 };
 
 export default theme;
