5852f0ebe7dfd7c5b9ab0a96c8926eb28477b243
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-08-12 18:59:56 -0300

    Refactor products layout

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,34 +1,53 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const ProductContainer = styled.div`
   display: flex;
   align-items: center;
   padding-top: 8px;
   text-align: left;
-  ul li {
-    text-decoration: none;
-    list-style: none;
-    font-size: 14px;
-  }
+  padding: 16px;
 `;
 const DescriptionContainer = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: 24px;
 `;
 
-const Product = ({ img, title, price, discountedPrice }) => (
-  <ProductContainer>
-    <ul>
-      <li>
-        <img src={img} alt="Imagem do Produto" width={100} />
-        <DescriptionContainer>
-          {title}
-          {price}
-          {discountedPrice}
-        </DescriptionContainer>
-      </li>
-    </ul>
-  </ProductContainer>
-);
+const ProductTitle = styled.h3`
+  margin: 8px 0;
+  font-size: 0.9rem;
+`;
+
+const ProductImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const Pricing = styled.p`
+  margin: 0 0 8px 0;
+  font-weight: 600;
+  ${(props) =>
+    props.listingPrice &&
+    css`
+      color: ${(props) => props.theme.disabledColorText};
+      font-size: 0.825rem;
+      margin-bottom: 0;
+    `}
+`;
+
+const Product = ({ img, title, price, sellingPrice }) => {
+
+  return (
+    <ProductContainer>
+      <ProductImg src={img} alt="Imagem do Produto" />
+      <DescriptionContainer>
+        <ProductTitle>{title}</ProductTitle>
+        <Pricing price>{price / 100}</Pricing>
+        <Pricing>{sellingPrice / 100}</Pricing>
+      </DescriptionContainer>
+    </ProductContainer>
+  );
+};
 
 export default Product;
